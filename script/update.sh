3af1d4cdac8778f07d458469b0e84f7d7156feac
Mustard Mustang <mustardmustang@example.com>
2012-03-23 10:59:59 -0400

    Update script is now aware of the move.

--- script/update.sh
+++ script/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd `dirname $0`
+cd `dirname $0`/..
 echo Updating remote submodules...
 git submodule update --init --recursive
 git submodule foreach "git fetch && git checkout origin/HEAD"
