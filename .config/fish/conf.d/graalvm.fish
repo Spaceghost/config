7b627f21841de604215e9a7ca378dd65de12b33a
Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>
2020-05-26 06:57:17 -0700

    Disable graalvm temporarily
    I've got to make a note to add these via their own graal_paths function to the
    paths. That is the way of the friendly interactive shell, after all.

--- .config/fish/conf.d/graalvm.fish
+++ .config/fish/conf.d/graalvm.fish
@@ -1,8 +1,8 @@
-set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/bin $PATH
-set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home 
-set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/js/bin/ $PATH
-set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/ruby/bin/ $PATH
-set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/python/bin/ $PATH
-set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/smalltalk/bin/ $PATH
+# set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/bin $PATH
+# set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home
+# set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/js/bin/ $PATH
+# set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/ruby/bin/ $PATH
+# set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/python/bin/ $PATH
+# set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/jre/languages/smalltalk/bin/ $PATH
 
-set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/
+# set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-java8-20.0.0/Contents/Home/
