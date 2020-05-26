d9f713e4aeda88ed5c86c178cef38ceeddc1c234
Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>
2020-05-26 06:42:09 -0700

    set MAKEFLAGS to reserve four and surrender the rest to compilers
    Save 4 cores because I don't have anything with less than 8 these days

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 source ~/.asdf/asdf.fish
 
-set -gx MAKEFLAGS -j8
+set -gx MAKEFLAGS -j (nproc --ignore=4)
 # if status --is-interactive
 #     set -g fish_user_abbreviations
 #     abbr -a mvl move-last-download
